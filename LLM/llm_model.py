629d6d9540ac672c1e284717c7de9102112d300c
arif-05 <arifcse949@gmail.com>
2026-08-31 14:51:39 +0600

    old fashioned llm integrated

---
+++ LLM/llm_model.py
@@ -0,0 +1,12 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3.6-flash"
+)
+
+result = llm.invoke("what is the capital of Bangladesh?")
+
+print(result)
